5659d47fe25d62692a245a1d2b88863540e4497d
Ruslan Tolstov <tolstov@mlsdev.com>
2019-12-15 23:03:25 +0200

    host

--- lib/rswag_schema_export/differ.rb
+++ lib/rswag_schema_export/differ.rb
@@ -1,6 +1,18 @@
+require 'json'
+
 module RswagSchemaExport
   class Differ
     def self.call(previews_schema, new_schema)
+      previews_schema_json = JSON.parse(File.read(previews_schema))
+      previews_schema_json["host"] = "localhost"
+
+      File.write(previews_schema, JSON.pretty_generate(previews_schema_json))
+
+      new_schema_json = JSON.parse(File.read(new_schema))
+      new_schema_json["host"] = "localhost"
+
+      File.write(new_schema, JSON.pretty_generate(new_schema_json))
+
       diff = ::Swagger::Diff::Diff.new(previews_schema, new_schema)
       changes = []
       report = ""
